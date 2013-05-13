09b7c1cf3abe6b3f8bcd2c7a2a513886404e00f3
Ferdi Nebiev <ferdi.nebiev@gmail.com>
2013-05-13 23:11:29 +0300

    Added missing .csproj files to the solution

---
+++ Baloons-Pop-2/TestBaloonsPop/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestBaloonsPop
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
